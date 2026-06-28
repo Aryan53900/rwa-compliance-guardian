fb5da0dc45acabf96b4022c68175c399443021e4
Aryan53900 <167440967+Aryan53900@users.noreply.github.com>
2026-06-28 23:36:30 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,9 +3,14 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
 
+  console.log("Current Token:", token);
+
   if (!token) {
+    console.log("Redirecting to Login...");
     return <Navigate to="/login" replace />;
   }
+
+  console.log("User Authenticated");
 
   return children;
 }
