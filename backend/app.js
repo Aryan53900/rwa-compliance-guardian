f14aa5034eb06d62ba552c1a5113ee2405989f0f
Aryan53900 <167440967+Aryan53900@users.noreply.github.com>
2026-07-03 23:36:50 +0530

    some changes in App.js

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,21 @@ const cors = require("cors");
 require("dotenv").config();
 
 const complianceRoutes = require("./routes/complianceRoutes");
-const authRoutes = require("./routes/authRoutes");
+//const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api", complianceRoutes);
+
+//app.use("/api/auth", authRoutes);
+app.use("/api/compliance", complianceRoutes);
 
 app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "RWA Compliance Guardian API Running 🚀",
-  });
+    res.json({
+        success: true,
+        message: "RWA Compliance Guardian API Running 🚀"
+    });
 });
-
-app.use("/api", complianceRoutes);
 
 module.exports = app;
