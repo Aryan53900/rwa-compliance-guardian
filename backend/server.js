6ffb92403c5b17f74572b2b5565b72ef1617fe66
Aryan53900 <167440967+Aryan53900@users.noreply.github.com>
2026-07-03 23:37:54 +0530

    Update the server.js , local host address new

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 RWA Compliance Guardian Backend running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`🚀 RWA Compliance Guardian Backend running on port ${PORT}`);
 });
